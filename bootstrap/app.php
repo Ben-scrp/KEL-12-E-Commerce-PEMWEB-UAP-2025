24826f2b1f2861eb9e25100701f9a20d00e6b2a0
Ben <beningning6@gmail.com>
2025-12-07 23:44:36 +0700

    dasboard admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,24 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+            // --- DAFTARKAN GROUP WEB ---
+        $middleware->group('web', [
+            \App\Http\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\VerifyCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ]);
+
+        // --- DAFTARKAN ALIAS MIDDLEWARE ---
+        $middleware->alias([
+            'auth' => \App\Http\Middleware\Authenticate::class,
+            'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+            // ⭐ INI MIDDLEWARE ROLE KAMU
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,31 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// =============================
+//   RBAC: Role Based Access Control
+// =============================
+
+// --- ADMIN ONLY ---
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return "Halaman Dashboard Admin";
+    })->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'role:seller'])->group(function () {
+    Route::get('/seller/dashboard', function () {
+        return "Halaman Dashboard Seller";
+    })->name('seller.dashboard');
+});
+
+Route::middleware(['auth', 'role:member'])->group(function () {
+    Route::get('/customer/home', function () {
+        return "Halaman Customer";
+    })->name('customer.home');
+});
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
